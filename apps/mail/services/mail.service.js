28abc2c04a9b91d7bf2bb624ea83081b84078832
bengold1999 <bengold1999@gmail.com>
2024-03-13 11:16:58 +0200

    Add all components and basic sata to services

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,39 @@
-// mail service
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyMail,
+    getDefaultFilter,
+    getFilterFromParams
+}
+
+
+const email = {//Model - start with a basic model of emails: 
+    id: 'e101',
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'momo@momo.com',
+    to: 'user@appsus.com'
+}
+
+const loggedinUser = {//have a basic user:
+
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+
+
+const criteria = {//emailService query function should get a criteria(filterBy
+    status: 'inbox/sent/trash/draft',
+    txt: 'puki', // no need to support complex text search
+    isRead: true, // (optional property, if missing: show all)
+    isStared: true, // (optional property, if missing: show all)
+    lables: ['important', 'romantic'] // has any of the labels
+}
