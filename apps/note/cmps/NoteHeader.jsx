eb572c2035ac0e42b186af0de92d5d47a2f2bc18
israel <lisrael1989@gmail.com>
2024-03-14 11:06:38 +0200

    Add header to Note page and add filter by txt and title

--- apps/note/cmps/NoteHeader.jsx
+++ apps/note/cmps/NoteHeader.jsx
@@ -1,5 +1,5 @@
 const { useState, useEffect } = React;
-const { Link } = ReactRouterDOM
+const { Link,NavLink } = ReactRouterDOM
 
 
 export function NoteHeader({ onSetFilter, filterBy }) {
@@ -24,27 +24,33 @@ export function NoteHeader({ onSetFilter, filterBy }) {
 
     return <header className="note-header">
       
-    
-
         <Link to="/note">
           <img
-            className="keep-img animate__animated animate__bounce"
+            className="keep-img-header animate__animated animate__bounce"
             src="assets\img\keep.png"
           ></img>
       </Link>
-       
-      <form onSubmit={onFilter}>
+
+     
+      <form className="search-header" onSubmit={onFilter}>
         <label htmlFor="title"></label>
         <input
           type="search"
           id="title"
+          className="title"
           name="title"
           value= {filterByToEdit.title} 
           onChange={handleChange}
           placeholder="Search..."
         />
-
       </form>
+        <img className="keep-btn" src="assets\img\keep-header-btn.png" alt="" />
+      {/* <nav>
+             <NavLink className="fa-solid fa-house" to="/"></NavLink>
+            <NavLink className="fa-solid fa-address-card" to="/about"></NavLink>
+            <NavLink className="fa-solid fa-envelope-open" to="/mail"></NavLink>
+            <NavLink className="fa-solid fa-note-sticky" to="/note"></NavLink>
+        </nav> */}
 
     </header>
 }
