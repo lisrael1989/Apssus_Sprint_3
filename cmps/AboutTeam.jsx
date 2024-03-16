22a249907dd2794e48824d3017ce8bf436e687ef
israel <lisrael1989@gmail.com>
2024-03-16 15:23:06 +0200

    Add social media buttons to about team page :linkedin and git url's

--- cmps/AboutTeam.jsx
+++ cmps/AboutTeam.jsx
@@ -3,17 +3,43 @@ export function Team() {
   return (
     <section>
       <h2 className="title-our-team">Meet the team</h2>
-  <div className="team-section">
-      <div className="team-member">
-        <h3 className="member-name">Israel Litvak</h3> 
-        <img className="member-img" src="assets/img/israel.jpg" alt="Israel Litvak" />
+        <div className="team-section">
+          <div className="team-member">
+            <h3 className="member-name">Israel Litvak</h3> 
+              <img className="member-img" src="assets/img/israel.jpg" alt="Israel Litvak" />
+
+          <div className="btn-social"> 
+          <a className="git-btn" href="https://github.com/lisrael1989" target="_blank"
+          ><i className="fa-brands fa-github"></i></a>
+
+         <a
+          className="linkedin-btn"
+          href="https://www.linkedin.com/in/israel-litvak-28baa682/" target="_blank">
+            <i className="fa-brands fa-linkedin"></i></a>
+
+
+          </div>
       </div>
       
-      <div className="team-member">
-        <h3 className="member-name">Ben Goldberger</h3> 
-        <img className="member-img" src="assets/img/ben.jpeg" alt="Ben Goldberger" />
+        <div className="team-member">
+         <h3 className="member-name">Ben Goldberger</h3> 
+          <img className="member-img" src="assets/img/ben.jpeg" alt="Ben Goldberger" />
+
+          <div className="btn-social"> 
+          <a className="git-btn" href="https://github.com/bengold1999" target="_blank">
+            <i className="fa-brands fa-github"></i></a>
+
+         <a
+          className="linkedin-btn"
+          href="https://www.linkedin.com/in/ben-goldberger-290b8b23a/" target="_blank" >
+            <i className="fa-brands fa-linkedin"></i></a>
+
+
+          </div>
+
       </div>
     </div>
     </section>
   );
 }
+
