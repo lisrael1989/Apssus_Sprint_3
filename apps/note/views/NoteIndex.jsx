95712fd5222b343a00acebb91a87010372bbfdc3
israel <lisrael1989@gmail.com>
2024-03-17 22:05:05 +0200

    add pin and unpin to notes page

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,142 +1,205 @@
 const { useState, useEffect } = React;
-import { noteService} from "./../services/note.service.js";
-import { utilService} from "../../../services/util.service.js";
+import { noteService } from "./../services/note.service.js";
+import { utilService } from "../../../services/util.service.js";
 import {
-    eventBusService,
-    showErrorMsg,
-    showSuccessMsg,
-  } from "../../../services/event-bus.service.js"
+  eventBusService,
+  showErrorMsg,
+  showSuccessMsg,
+} from "../../../services/event-bus.service.js";
 
 import { NotePreview } from "./NotePreview.jsx";
 import { UserMsg } from "../cmps/UserMsg.jsx";
 import { NoteHeader } from "../cmps/NoteHeader.jsx";
 import { AddNotes } from "../cmps/AddNotes.jsx";
-import { TodoNote} from "../cmps/TodoNote.jsx";
+import { TodoNote } from "../cmps/TodoNote.jsx";
 
+export function NoteIndex() {
+  const [notes, setNotes] = useState([]);
+  const [filterBy, setFilterBy] = useState(noteService.getDefaultFilter());
+  const [userMsg, setUserMsg] = useState("");
 
-    export function NoteIndex() {
-    const [notes, setNotes] = useState([]);
-    const [filterBy, setFilterBy] = useState(noteService.getDefaultFilter());
-    const [userMsg, setUserMsg] = useState("");
+  useEffect(() => {
+    loadNotes();
+  }, [filterBy]);
 
-    useEffect(() => {
-        loadNotes()
-    }, [filterBy])
+  function loadNotes() {
+    noteService.query(filterBy).then((notes) => {
+      setNotes(notes);
+    });
+  }
 
+  function onRemoveNote(noteId) {
+    noteService
+      .remove(noteId)
+      .then(() => {
+        setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
 
-    function loadNotes() {
-        noteService.query(filterBy)
-            .then((notes) => {
-                setNotes(notes)
-            })
-    }
-
-    function onRemoveNote(noteId) {
-        noteService
-          .remove(noteId)
-          .then(() => {
-            setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
-            
-            showSuccessMsg(`note removed successfully (${noteId})`);
-          })
-          .catch((err) => {
-            console.log("Had issues removing note", err);
-            showErrorMsg(`note removed successfully (${noteId})`);
-          });
-      }
-
-      function onDuplicateNote(noteToDuplicate) {
-        const newNote = { ...noteToDuplicate, id: "", createdAt: Date.now() };
-        
-        noteService.save(newNote).then(savedNote => {
-          setNotes(prevNotes => [...prevNotes, savedNote]);
-          showSuccessMsg('Note duplicated successfully');
-        }).catch(err => {
-          console.error('Error duplicating note', err);
-          showErrorMsg('Error duplicating note');
-        });
-      }
-      
-    
-
-
-      function onUpdateNoteColor(noteId, color) {
-        console.log(`Updating note ${noteId} with color ${color}`); 
-    
-        const updatedNotes = notes.map((note) => {
-            if (note.id === noteId) {
-                console.log('Found note to update:', note); 
-                return { ...note, style: { ...note.style, backgroundColor: color } };
-            }
-            return note;
-        });
-        console.log('Updated notes array:', updatedNotes); 
-        setNotes(updatedNotes);
-    
-        const noteToUpdate = updatedNotes.find(note => note.id === noteId);
-        noteService.save(noteToUpdate).then(() => {
-            console.log('Note color updated successfully in service'); 
-            loadNotes(); 
-        }).catch(err => console.error('Failed to save note color change', err));
-    }
-    
-    function onAddNote(newNote) {
-      noteService.save(newNote).then(savedNote => {
-          setNotes((prevNotes) => [...prevNotes, savedNote]);
-          showSuccessMsg('Note added successfully');
-      }).catch(err => {
-          console.error('Error adding note', err);
-          showErrorMsg('Error adding note');
+        showSuccessMsg(`note removed successfully (${noteId})`);
+      })
+      .catch((err) => {
+        console.log("Had issues removing note", err);
+        showErrorMsg(`note removed successfully (${noteId})`);
       });
-    }
+  }
 
-      function onSetFilter(fieldsToUpdate) {
-        setFilterBy((prevFilter) => ({ ...prevFilter, ...fieldsToUpdate }));
+  function onDuplicateNote(noteToDuplicate) {
+    const newNote = { ...noteToDuplicate, id: "", createdAt: Date.now() };
+
+    noteService
+      .save(newNote)
+      .then((savedNote) => {
+        setNotes((prevNotes) => [...prevNotes, savedNote]);
+        showSuccessMsg("Note duplicated successfully");
+      })
+      .catch((err) => {
+        console.error("Error duplicating note", err);
+        showErrorMsg("Error duplicating note");
+      });
+  }
+
+  function onUpdateNoteColor(noteId, color) {
+    console.log(`Updating note ${noteId} with color ${color}`);
+
+    const updatedNotes = notes.map((note) => {
+      if (note.id === noteId) {
+        console.log("Found note to update:", note);
+        return { ...note, style: { ...note.style, backgroundColor: color } };
       }
+      return note;
+    });
+    console.log("Updated notes array:", updatedNotes);
+    setNotes(updatedNotes);
 
+    const noteToUpdate = updatedNotes.find((note) => note.id === noteId);
+    noteService
+      .save(noteToUpdate)
+      .then(() => {
+        console.log("Note color updated successfully in service");
+        loadNotes();
+      })
+      .catch((err) => console.error("Failed to save note color change", err));
+  }
 
-      function flashMsg(txt) {
-        setUserMsg(txt);
-        setTimeout(() => {
-          setUserMsg("");
-        }, 3000);
+  function onAddNote(newNote) {
+    noteService
+      .save(newNote)
+      .then((savedNote) => {
+        setNotes((prevNotes) => [...prevNotes, savedNote]);
+        showSuccessMsg("Note added successfully");
+      })
+      .catch((err) => {
+        console.error("Error adding note", err);
+        showErrorMsg("Error adding note");
+      });
+  }
+
+  function onSetFilter(fieldsToUpdate) {
+    setFilterBy((prevFilter) => ({ ...prevFilter, ...fieldsToUpdate }));
+  }
+
+  function flashMsg(txt) {
+    setUserMsg(txt);
+    setTimeout(() => {
+      setUserMsg("");
+    }, 3000);
+  }
+
+  function handleUpdateNote(updatedNote) {
+    const updatedNotes = notes.map((note) =>
+      note.id === updatedNote.id ? updatedNote : note
+    );
+    // setNotes(updatedNotes);
+
+    noteService
+      .save(updatedNote)
+      .then(() => {
+        console.log("Note updated successfully");
+      })
+      .catch((err) => {
+        console.error("Failed to update note", err);
+      });
+  }
+
+  function onTogglePin(noteId) {
+   
+    const updatedNotes = notes.map((note) => {
+      if (note.id === noteId) {
+      
+        return { ...note, isPinned: !note.isPinned };
       }
+      return note;
+    });
+    setNotes(updatedNotes); 
+  
+   
+    noteService.save(updatedNotes.find(note => note.id === noteId))
+      .then(() => {
+        showSuccessMsg('Note pin status toggled successfully');
+      })
+      .catch((err) => {
+        console.error('Failed to save note pin status change', err);
+        showErrorMsg('Error toggling note pin status');
+      });
+  }
+  
 
 
-      function handleUpdateNote(updatedNote) {
-        const updatedNotes = notes.map(note => note.id === updatedNote.id ? updatedNote : note);
-        setNotes(updatedNotes);
-    
-        noteService.save(updatedNote).then(() => {
-            console.log('Note updated successfully');
-        }).catch(err => {
-            console.error('Failed to update note', err);
-        });
-    }
-
-    return (
-
+if (!notes) {
+  return <div>load</div>
+}
+  return (
     <section className="note-index">
-        <NoteHeader
-            onSetFilter={onSetFilter}
-            filterBy={{ filterBy }}/>
+      <NoteHeader onSetFilter={onSetFilter} filterBy={{ filterBy }} />
 
-        <AddNotes 
-        onAddNote={onAddNote}  
+      <AddNotes onAddNote={onAddNote} />
+
+      {notes && notes.some((note) => note.isPinned) && (
+      <div>
+    <h2>Pinned</h2>
+    <React.Fragment>
+      <NotePreview
+        notes={notes.filter((note) => note.isPinned)}
+        onRemoveNote={onRemoveNote}
+        onDuplicateNote={onDuplicateNote}
+        onUpdateNoteColor={onUpdateNoteColor}
+        onUpdateNote={handleUpdateNote}
+        onTogglePin={onTogglePin}
+      />
+    </React.Fragment>
+      </div>
+)}
+
+{/* {notes && notes.some(note => note.isPinned) && <hr className='line-hr' />} */}
+
+{notes && notes.some((note) => !note.isPinned) && (
+      <div>
+    <h2>Others</h2>
+    <React.Fragment>
+      <NotePreview
+        notes={notes.filter((note) => !note.isPinned)}
+        onRemoveNote={onRemoveNote}
+        onDuplicateNote={onDuplicateNote}
+        onUpdateNoteColor={onUpdateNoteColor}
+        onUpdateNote={handleUpdateNote}
+        onTogglePin={onTogglePin}
         
-        />
+      />
+    </React.Fragment>
+      </div>
+)}
 
+      
+      {/* <NotePreview
+        notes={notes}
+        onRemoveNote={onRemoveNote}
+        onDuplicateNote={onDuplicateNote}
+        onUpdateNoteColor={onUpdateNoteColor}
+        onUpdateNote={handleUpdateNote}
+        onTogglePin={onTogglePin}
 
+      /> */}
 
-        <NotePreview
-            notes={notes}
-            onRemoveNote={onRemoveNote}
-            onDuplicateNote={onDuplicateNote}
-            onUpdateNoteColor={onUpdateNoteColor}
-            onUpdateNote={handleUpdateNote}
-            />
-    
-        <UserMsg msg={userMsg} />
-        </section>
-    )
+      <UserMsg msg={userMsg} />
+    </section>
+  );
 }
